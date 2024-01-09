f042feb7d4b84992a96c39aab5ccde5671178263
Kevalpaneliya <141860450+Kevalpaneliya@users.noreply.github.com>
2024-01-09 17:12:06 +0000

    Datatypes

---
+++ 01_node_basic/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // java Script na nava rules pramane code kam kare etle aa line lakhva ma ave chhe
+
+// alert (3 + 3) // node js no use karyo chhe ane karvano chhe browser no use karvanao nathi ane karyo nathi
+
+console.log (3
+    +4) // code bane tya sudhi line ma j lakhvo 
+console.log("keval Paneliya")
+console.log("----------------------===============-----------------")
+let name = "Keval Rajubhai Paneliya"
+let age = 31
+let isLoggedIn = false
+let state ;
+
+console.log(typeof name);
+
+// Number => 2 to power 53
+// bigint
+// String => ""
+// boolean => True / False
+// null => standalone Value
+// undefined
+// Symbol => unique
+
+// object
+console.log("----------------------===============-----------------")
+console.log(typeof undefined); //ndefined
+console.log(typeof null); //object
---
+++ 01_node_basic/variable.js
@@ -0,0 +1,14 @@
+const accountId = 123456;
+let acconuntEmailid = "kevalpaneliya@gmail.com"
+var accountPasswprd = "123852"
+accountCity = "Surat"
+
+
+// accountId = 123
+
+acconuntEmailid = "hc@hc.com"
+accountPasswprd = "12121212"
+accountCity = "Bardoli"
+
+ console.log(accountId);
+ console.table([acconuntEmailid, accountId, accountPasswprd, accountCity]);
