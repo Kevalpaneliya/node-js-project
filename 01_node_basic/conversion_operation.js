1a88bc7102e2e403609d98a00670cee9a74a101d
Kevalpaneliya <141860450+Kevalpaneliya@users.noreply.github.com>
2024-01-13 16:55:37 +0000

    convertion

---
+++ 01_node_basic/conversion_operation.js
@@ -0,0 +1,20 @@
+let score = "55kk"
+console.log(typeof score);
+console.log(typeof (score));
+
+// score ni value ne number ma convert kari nakhe 
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let temp = "keval"
+console.log(typeof temp);
+
+let bool = Boolean(temp);
+console.log(bool);
+
+let somenumber = 33;
+console.log(somenumber);
+let stringnumber = String (somenumber)
+console.log(typeof stringnumber);
